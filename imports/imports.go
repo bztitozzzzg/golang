3461ea6d43369d7d6590bf9b5bd0cc3a6c9dfd22
itotakehiro4 <itotakehiro4@gmail.com>
2022-10-10 00:04:39 +0900

    Add comment

--- imports/imports.go
+++ imports/imports.go
@@ -1,3 +1,15 @@
+/*
+Imports
+このコードでは、括弧でパッケージのインポートをグループ化し、factoredインポートステートメント( factored import statement )としています。
+
+もちろん、複数のインポートステートメントで書くこともできます:
+
+import "fmt"
+import "math"
+ですが、先に示したfactoredインポートステートメントの方がより良いスタイルです。
+
+訳注: ここの factored の意味は、「要素化、グループ化、整理済み」ということです。良い日本語募集中。
+*/
 package main
 
 import (
--- multiple-results/multiple-results.go
+++ multiple-results/multiple-results.go
@@ -1,3 +1,9 @@
+/*
+Multiple results
+関数は複数の戻り値を返すことができます。
+
+この swap 関数は2つの string を返します。 とても簡単に交換できますね！
+*/
 package main
 
 import "fmt"
